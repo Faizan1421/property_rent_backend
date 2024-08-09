6b60a12ad0294e1162c09f4747099934cc15d837
Faizan Tayyab <faizantayyab37@gmail.com>
2024-08-09 19:54:36 +0500

    auth middleware added

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,35 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    console.log(token);
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    if (error instanceof ApiError) {
+      throw error; // Preserve the original status code and message
+    }
+    throw new ApiError(500, error?.message || "Internal Server Error");
+  }
+});
